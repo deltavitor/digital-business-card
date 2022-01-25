04f978e4c3e2d7a8916419b0a182d92bf4c9a19d
deltavitor <cvitor1618@gmail.com>
2022-01-25 09:59:38 -0300

    Updated Footer component with content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
+import GitHub from '../images/icons/github.svg'
+import LeetCode from '../images/icons/leetcode.svg'
+
 function Footer() {
   return (
-    <h1>Footer</h1>
+    <div>
+      <img src={GitHub} onClick={() => window.location = 'https://github.com/deltavitor'}/>
+      <img src={LeetCode} onClick={() => window.location = 'https://leetcode.com/Deltavitor/'}/>
+    </div>
   )
 }
 
