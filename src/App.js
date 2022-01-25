e6244aea07a18c6a5e3e7b01977f165fb248bbf3
deltavitor <cvitor1618@gmail.com>
2022-01-24 21:24:41 -0300

    Created App component and rendering in index.js

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Info from './components/Info.js'
+import About from './components/About.js'
+import Interests from './components/Interests.js'
+import Footer from './components/Footer.js'
+
+function App() {
+  return (
+    <div>
+      <Info />
+      <About />
+      <Interests />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
