a00787c34317b938f70534ff77e36c68e29139fe
deltavitor <cvitor1618@gmail.com>
2022-01-25 09:58:42 -0300

    Updated About component with content

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function About() {
   return (
-    <h1>About</h1>
+    <div>
+      <h2>About</h2>
+      <p>I'm a Computer Science student from Brazil, currently a product design intern. I'm a developer in my free time, and I'm particularly interested in all things development related, from front and back-end to competitive programming. I also like to incorporate human-centered in what I do as developer, and am constantly looking for new things to learn.</p>
+    </div>
   )
 }
 
