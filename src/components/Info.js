9e185bde36bc90aa52f3178210a3abe55a30fbb9
deltavitor <cvitor1618@gmail.com>
2022-01-25 09:58:04 -0300

    Updated Info component with content

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,8 +1,28 @@
 import React from 'react'
 
+import profilePicture from '../images/my-duckling.jpg'
+import LinkedIn from '../images/icons/linkedin.svg'
+import Mail from '../images/icons/mail.svg'
+
 function Info() {
   return (
-    <h1>Info</h1>
+    <div>
+      <div>
+        <img src={profilePicture}/>
+        <h1>Vítor Costa</h1>
+        <h3>Product Design Intern and Student</h3>
+      </div>
+      <div>
+        <button onClick={() => window.location = 'mailto:cvitor1618@gmail.com'}>
+          <span><img src={Mail}/></span>
+          <span>Email</span>
+        </button>
+        <button onClick={() => window.location = 'https://www.linkedin.com/in/vitorhugoc/'}>
+          <span><img src={LinkedIn} /></span>
+          <span>LinkedIn</span>
+        </button>
+      </div>
+    </div>
   )
 }
 
