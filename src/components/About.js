98ef7b173ec025237bb3d5445f457afc1433470d
deltavitor <cvitor1618@gmail.com>
2022-01-30 21:39:21 -0300

    Stylized About and Interests components

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function About() {
   return (
-    <div>
+    <div className="about">
       <h2>About</h2>
       <p>I'm a Computer Science student from Brazil, currently a product design intern. I'm a developer in my free time, and I'm particularly interested in all things development related, from front and back-end to competitive programming. I also like to incorporate human-centered in what I do as developer, and am constantly looking for new things to learn.</p>
     </div>
--- src/components/Interests.js
+++ src/components/Interests.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Interests() {
   return (
-    <div>
+    <div className="interests">
       <h2>Interests</h2>
       <p>Ducks. Grand strategy games. Gardening and hydroponics. Competitive programming. Travelling. Oh, have I told you I have two pet ducks?</p>
     </div>
